625faabba40a2dfd5c2ebcebd8941f8c6d094afe
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-13 16:41:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,9 +108,11 @@ def changing_alan
         }
      }
     
-   alans_new_info = "GUI"
+  # alans_new_info = "GUI"
      
-    programmer_hash[:alan_kay][:known_for]  = alans_new_info
+  #   programmer_hash[:alan_kay][:known_for]  = alans_new_info
+    
+    alans_new_info
      
      
 end
